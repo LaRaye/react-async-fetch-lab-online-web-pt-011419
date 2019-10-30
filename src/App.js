0cb217c9407feaf90889bcfad26eba8359b6bc4a
LaRaye Johnson <lnj2105@gmail.com>
2019-10-30 20:10:53 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -11,8 +11,14 @@ export default class App extends Component {
   }
   
   componentDidMount() {
-    
-  }
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          peopleInSpace: data.people
+        })
+      })
+  } 
   
   render() {
     return (
