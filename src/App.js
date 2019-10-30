356e9e445e6e042f2ebb798e2c6582d92d5576a1
LaRaye Johnson <lnj2105@gmail.com>
2019-10-30 20:10:13 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  
   constructor() {
     super()
     
     state = {
       peopleInSpace: []
     }
+  }
+  
+  componentDidMount() {
+    
   }
   
   render() {
